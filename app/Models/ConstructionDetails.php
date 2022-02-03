beb35d8097edf6aa605d5bcf867b7e35247beb4a
Rishabh Choudhary <61964079+Ch-rishabh@users.noreply.github.com>
2022-02-03 14:15:18 +0530

    Updates

--- app/Models/ConstructionDetails.php
+++ app/Models/ConstructionDetails.php
@@ -109,7 +109,7 @@ class ConstructionDetails extends Model
         $return['total_records'] = ConstructionDetails::whereNull('deleted_at')->where('project_id',$request['project_id'])->where('block_id',$request['block_id'])->distinct()->count('main_description_id');
 
         $data = ConstructionDetails::join('main_descritpions', 'main_descritpions.id', '=', 'construction_details.main_description_id')
-        ->select('construction_details.main_description_id','construction_details.apartment_id','main_descritpions.description as description_header',DB::raw("CASE WHEN sum(construction_details.total) IS NULL THEN 0 WHEN sum(construction_details.amount_booked) IS NULL THEN ROUND(sum(construction_details.total),2) ELSE ROUND(sum(construction_details.total)-sum(construction_details.amount_booked),2) END as remaining_booking_amount"))->whereNull('construction_details.deleted_at')
+        ->select('construction_details.main_description_id','construction_details.apartment_id','construction_details.floor_id','main_descritpions.description as description_header',DB::raw("CASE WHEN sum(construction_details.total) IS NULL THEN 0 WHEN sum(construction_details.amount_booked) IS NULL THEN ROUND(sum(construction_details.total),2) ELSE ROUND(sum(construction_details.total)-sum(construction_details.amount_booked),2) END as remaining_booking_amount"))->whereNull('construction_details.deleted_at')
         ->where('construction_details.project_id',$request['project_id'])
         ->where('construction_details.block_id',$request['block_id'])
         ->groupBy('description_header','construction_details.main_description_id','construction_details.apartment_id');
