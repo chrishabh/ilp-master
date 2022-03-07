bc2a39487648b6e646d6569500984b0565d4bec1
Rishabh Choudhary <61964079+Ch-rishabh@users.noreply.github.com>
2022-03-07 16:37:53 +0530

    updates

--- app/Helpers/custom_helper.php
+++ app/Helpers/custom_helper.php
@@ -399,27 +399,27 @@ if (! function_exists('envparam')) {
                             ];
                             foreach($row_data as $cell_key => $cell_value)
                             {
-                                if($cell_key == '3'){
+                                if($cell_key == '2'){
                                     $insert_data['description'] = (!empty($cell_value))?"'".str_replace("'","''",$cell_value)."'":NULL;
-                                }elseif($cell_key == '4'){
+                                }elseif($cell_key == '3'){
                                     $insert_data['area'] = (!empty($cell_value))?$cell_value:0;
-                                }elseif($cell_key == '7'){
+                                }elseif($cell_key == '4'){
                                     $insert_data['unit'] = (!empty($cell_value))?$cell_value:NULL;
                                 }elseif($cell_key == '5'){
                                     $insert_data['lab_rate'] = (!empty($cell_value))?ltrim(trim($cell_value," "),'£'):NULL;
-                                }elseif($cell_key == '6'){
+                                }elseif($cell_key == '5'){
                                     $insert_data['total'] = (!empty($cell_value))?ltrim(trim($cell_value," "),'£'):NULL;
-                                }elseif($cell_key == '8'){
+                                }elseif($cell_key == '6'){
                                     $insert_data['amount_booked'] = (!empty($cell_value))?"'".ltrim(trim($cell_value," "),'£')."'":NULL;
-                                }elseif($cell_key == '9'){
+                                }elseif($cell_key == '7'){
                                     $insert_data['name'] = (!empty($cell_value))?"'".$cell_value."'":NULL;
-                                }elseif($cell_key == '10'){
+                                }elseif($cell_key == '8'){
                                     $insert_data['wages'] = (!empty($cell_value))?"'".$cell_value."'":NULL;
-                                }elseif($cell_key == '11'){
+                                }elseif($cell_key == '9'){
                                     $insert_data['quantity'] = (!empty($cell_value))?"'".$cell_value."'":NULL;
-                                }elseif($cell_key == '12'){
+                                }elseif($cell_key == '10'){
                                     $insert_data['booking_description'] = (!empty($cell_value))?"'".str_replace("'","''",$cell_value)."'":NULL;
-                                }elseif($cell_key == '13'){
+                                }elseif($cell_key == '11'){
                                     $insert_data['floor'] = $floor_name??null;
                                 }
                                 
