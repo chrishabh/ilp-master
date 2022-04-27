4dc5c10674bc6d2ca30c1dec3a154d02df98a710
Rishabh Choudhary <61964079+Ch-rishabh@users.noreply.github.com>
2022-04-28 01:54:48 +0530

    fix

--- database/seeders/LookUpValueDetails.php
+++ database/seeders/LookUpValueDetails.php
@@ -18,11 +18,11 @@ class LookUpValueDetails extends Seeder
         LookUpValue::truncate();
         DB::insert("INSERT INTO `look_up_values` ( `module`, `type`, `key`, `text`, `sort_order`, `deleted_at`, `created_at`, `updated_at`) VALUES 
         ('wages', 'trade', 'Fire Stop', 'Fire Stop', '0', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-        ('wages', 'trade', 'Fixer', 'Fixer', '1', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-        ('wages', 'trade', 'Labour', 'Labour', '2', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-        ('wages', 'trade', 'Making Good', 'Making Good', '3', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-        ('wages', 'trade', 'Plaster/Render', 'Plaster/Render', '4', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-        ('wages', 'trade', 'Screed', 'Screed', '5', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-        ('wages', 'trade', 'SFS', 'SFS', '6', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);");
+        ,('wages', 'trade', 'Fixer', 'Fixer', '1', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+        ,('wages', 'trade', 'Labour', 'Labour', '2', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+        ,('wages', 'trade', 'Making Good', 'Making Good', '3', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+        ,('wages', 'trade', 'Plaster/Render', 'Plaster/Render', '4', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+        ,('wages', 'trade', 'Screed', 'Screed', '5', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+        ,('wages', 'trade', 'SFS', 'SFS', '6', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);");
     }
 }
