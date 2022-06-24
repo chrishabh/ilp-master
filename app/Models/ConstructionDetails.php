bed7d0265b618b8438cf1be88e61be9a8b0e2dec
Rishabh Choudhary <61964079+Ch-rishabh@users.noreply.github.com>
2022-06-24 16:46:37 +0530

    updates

--- app/Models/ConstructionDetails.php
+++ app/Models/ConstructionDetails.php
@@ -78,7 +78,7 @@ class ConstructionDetails extends Model
                 
                 foreach($data->toArray() as $records){
                     $sub_final['records'][] =  $records;
-                    $total += (int)$records['total'] ;
+                    $total += (float)$records['total'] ;
                     $res = explode(',',str_replace("'", "", $records['amount_booked']));
                     $total_amount_booked +=  array_sum($res);
                 }
