67d44bae66044cdde55bab2e255c8c6082b9a047
Rishabh Choudhary <61964079+Ch-rishabh@users.noreply.github.com>
2022-10-04 17:45:52 +0530

    Revert "Update"
    This reverts commit 75b34d802f7bbeba499eb57e8a8480935942504f.

--- app/Models/ConstructionDetails.php
+++ app/Models/ConstructionDetails.php
@@ -120,7 +120,7 @@ class ConstructionDetails extends Model
         $return['total_records'] = ConstructionDetails::whereNull('deleted_at')->where('project_id',$request['project_id'])->where('block_id',$request['block_id'])->distinct()->count('main_description_id');
 
         $data = ConstructionDetails::join('main_descritpions', 'main_descritpions.id', '=', 'construction_details.main_description_id')
-        ->select('construction_details.main_description_id','construction_details.apartment_id','construction_details.floor_id','main_descritpions.description as description_header',DB::raw("CASE WHEN sum(construction_details.total) IS NULL THEN 0 ELSE ROUND(sum(construction_details.total),2) END as remaining_booking_amount"))->whereNull('construction_details.deleted_at')
+        ->select('construction_details.main_description_id',$field,'main_descritpions.description as description_header',DB::raw("CASE WHEN sum(construction_details.total) IS NULL THEN 0 ELSE ROUND(sum(construction_details.total),2) END as remaining_booking_amount"))->whereNull('construction_details.deleted_at')
         ->where('construction_details.project_id',$request['project_id'])
         ->where('construction_details.block_id',$request['block_id']);
 
@@ -173,7 +173,6 @@ class ConstructionDetails extends Model
                 if(count($apartment_id)>0){
                     //$sub_description []
                     $sub_final['apartment_id'] = $value['apartment_id'];
-                    $sub_final['floor_id'] = $value['floor_id'];
                     $booked_amount = ConstructionDetails::select('amount_booked')->whereNull('construction_details.deleted_at')
                     ->where('construction_details.project_id',$request['project_id'])->where('construction_details.main_description_id',$value['main_description_id'])
                     ->where('construction_details.sub_description_id',$sub_value['sub_description_id'])
