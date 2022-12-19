20329b6e0a2b6fa0fcd0389e410c5bf0f0484c1c
Rishabh Choudhary <61964079+Ch-rishabh@users.noreply.github.com>
2022-12-19 17:39:02 +0530

    updates in wages excel and limit

--- app/Models/ApartmentDetails.php
+++ app/Models/ApartmentDetails.php
@@ -18,7 +18,7 @@ class ApartmentDetails extends Model
 
     public static function getApartmentDetails($request)
     {
-        $noOfRecord =  130;
+        $noOfRecord =  700;
         $current_page = $request['page_number'] ?? 1;
         $offset = ($current_page*$noOfRecord)-$noOfRecord;
 
