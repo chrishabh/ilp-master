8ff5924548b7e0d7d83f96f54b44cd69afde4653
Rishabh Choudhary <61964079+Ch-rishabh@users.noreply.github.com>
2023-05-26 19:17:01 +0530

    Update

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::middleware(['middleware'=> 'tracker'],function () {
+Route::middleware(['middleware'=> ['tracker']],function () {
         Route::group(['namespace' => 'Api\Auth'], function () {
           
                 Route::post('login','UserController@userLogin');		
