251928eb147d1703515cb79521247cf53b70109e
rchoudhary-at-770608018768 <ch.rishabh8527@gmail.com>
2024-05-26 23:56:04 +0530

    update

--- app/Models/MainDescritpion.php
+++ app/Models/MainDescritpion.php
@@ -105,7 +105,7 @@ class MainDescritpion extends Model
         $current_page = $request['page_no'] ?? 1;
         $offset = ($current_page*$noOfRecord)-$noOfRecord;
 
-        $return = MainDescritpion::whereNull('deleted_at')->distinct()->offset($offset)->limit($noOfRecord)->get();
+        $return = MainDescritpion::select('id','description')->whereNull('deleted_at')->distinct()->offset($offset)->limit($noOfRecord)->get();
 
         if(count($return)>0){
             return $return->toArray();
--- app/Models/SubDescritpion.php
+++ app/Models/SubDescritpion.php
@@ -59,7 +59,7 @@ class SubDescritpion extends Model
         $current_page = $request['page_no'] ?? 1;
         $offset = ($current_page*$noOfRecord)-$noOfRecord;
 
-        $return = SubDescritpion::whereNull('deleted_at')->distinct()->offset($offset)->limit($noOfRecord)->get();
+        $return = SubDescritpion::select('id','sub_description')->whereNull('deleted_at')->distinct()->offset($offset)->limit($noOfRecord)->get();
 
         if(count($return)>0){
             return $return->toArray();
