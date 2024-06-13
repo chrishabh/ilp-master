e817c8dab7db222f382831e7d7c888c45cc7a61c
rchoudhary-at-770608018768 <ch.rishabh8527@gmail.com>
2024-06-14 01:25:31 +0530

    update

--- app/Helpers/custom_helper.php
+++ app/Helpers/custom_helper.php
@@ -462,8 +462,8 @@ if (! function_exists('envparam')) {
                                         if($cell_key == '5'){
                                             $insert_data['description'] = (!empty($cell_value))?"'".str_replace("'","''",$cell_value)."'":NULL;
                                         }elseif($cell_key == '6'){
-                                            $insert_data['area'] = (!empty($cell_value))?$cell_value:0;
-                                            $insert_data['total'] = (!empty($cell_value))?$cell_value:0;
+                                            $insert_data['area'] = (!empty($cell_value))? preg_replace('-/[^\d.]/', '0',$cell_value):0;
+                                            $insert_data['total'] = (!empty($cell_value))? preg_replace('-/[^\d.]/', '0',$cell_value):0;
                                         }elseif($cell_key == '7'){
                                             $insert_data['unit'] = (!empty($cell_value))?$cell_value:NULL;
                                         }elseif($cell_key == '8'){
