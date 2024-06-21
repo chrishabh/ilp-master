d1283e38b48d47756b3f6b78b14267dfe8101d8d
rchoudhary-at-770608018768 <ch.rishabh8527@gmail.com>
2024-06-22 03:34:59 +0530

    Update for release 2

--- app/Models/ConstructionDetails.php
+++ app/Models/ConstructionDetails.php
@@ -252,33 +252,21 @@ class ConstructionDetails extends Model
 
 
         $return['total_records'] = ConstructionDetails::whereNull('deleted_at')->where('project_id',$request['project_id'])->where('block_id',$request['block_id'])->distinct()->count('main_description_id');
+       
+        $data = ConstructionDetails::join('main_descritpions', 'main_descritpions.id', '=', 'construction_details.main_description_id')
+        ->select('construction_details.main_description_id','main_descritpions.description as description_header')->whereNull('construction_details.deleted_at')
+        ->where('construction_details.project_id',$request['project_id'])
+        ->where('construction_details.block_id',$request['block_id']);
+        
+        $data = $data->groupBy('description_header','construction_details.main_description_id')->whereNull('construction_details.apartment_id')->whereIn('construction_details.apartment_id',$request['apartment_id'])->whereIn('construction_details.floor_id',$request['floor_id'])->get();
 
-        if(count($apartment_id)>0){
-            $data = ConstructionDetails::join('main_descritpions', 'main_descritpions.id', '=', 'construction_details.main_description_id')
-            ->select('construction_details.main_description_id','construction_details.apartment_id','construction_details.floor_id','main_descritpions.description as description_header',DB::raw("CASE WHEN sum(construction_details.total) IS NULL THEN 0 ELSE ROUND(sum(construction_details.total),2) END as remaining_booking_amount"))->whereNull('construction_details.deleted_at')
-            ->where('construction_details.project_id',$request['project_id'])
-            ->where('construction_details.block_id',$request['block_id']);
-        }else{
-            $data = ConstructionDetails::join('main_descritpions', 'main_descritpions.id', '=', 'construction_details.main_description_id')
-            ->select('construction_details.main_description_id','construction_details.floor_id','main_descritpions.description as description_header',DB::raw("CASE WHEN sum(construction_details.total) IS NULL THEN 0 ELSE ROUND(sum(construction_details.total),2) END as remaining_booking_amount"))->whereNull('construction_details.deleted_at')
-            ->where('construction_details.project_id',$request['project_id'])
-            ->where('construction_details.block_id',$request['block_id']);
-        }
-
-        if(count($apartment_id)>0){
-            $data = $data->groupBy('description_header','construction_details.main_description_id','construction_details.apartment_id','construction_details.floor_id')->whereIn('construction_details.apartment_id',$request['apartment_id'])->get();
-        }else{
-            $data = $data->groupBy('description_header','construction_details.main_description_id','construction_details.floor_id',)->whereNull('construction_details.apartment_id')->whereIn('construction_details.floor_id',$request['floor_id'])->get();
-        }
         
 
         if(count($data)>0){
             $records = $data->toArray();
         }
-        $array  = $booked_array = [];
+   
         foreach($records as $value){
-           $sub_final = [];
-                $units = null;
                 $description_data = ConstructionDetails::whereNull('construction_details.deleted_at')
                 ->where('construction_details.project_id',$request['project_id'])->where('construction_details.main_description_id',$value['main_description_id'])
                 ->where('construction_details.block_id',$request['block_id']);
@@ -302,19 +290,6 @@ class ConstructionDetails extends Model
 
             $final [] = $response;
         }
-        // $main_description = MainDescritpion::getDistinctDescription();
-        // foreach($main_description as $value){
-        //     if(isset($response[$value['description_header']])){
-        //         if(isset($array[$value['description_header']])){
-        //             $response[$value['description_header']]['total'] = array_sum($array[$value['description_header']]);
-        //             $response[$value['description_header']]['remaining_total'] = array_sum($array[$value['description_header']]) - array_sum($booked_array[$value['description_header']]);
-        //         } else {
-        //             $response[$value['description_header']]['total'] = 0;
-        //         }
-                
-        //         $final[] =  $response[$value['description_header']];
-        //     }
-        // }
 
         $return['description_work_details'] =  $final;
 
