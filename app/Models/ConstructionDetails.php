ca1f15245c057fe33007f190e5bb32e1a73b4dcc
rchoudhary-at-770608018768 <ch.rishabh8527@gmail.com>
2024-06-24 20:08:06 +0530

    update

--- app/Models/ConstructionDetails.php
+++ app/Models/ConstructionDetails.php
@@ -293,11 +293,12 @@ class ConstructionDetails extends Model
                         $sub_records [] = $const_value;
                        
                     }
-                    $construct_data['remaining_booking_amount'] =  $total_area - $total_amount_booked;
-                    $construct_data['total'] =  $total_area;
-                    $construct_data['unit'] =    $unit;
+                 
                     $sub_response[$value['description_header']]['description'] = $desc_value['description'];
-                    $sub_response[$value['description_header']]['sub_records'][] = $sub_records;
+                    $sub_response[$value['description_header']]['sub_records'] = $sub_records;
+                    $sub_response[$value['description_header']]['remaining_booking_amount'] =  $total_area - $total_amount_booked;
+                    $sub_response[$value['description_header']]['total'] =  $total_area;
+                    $sub_response[$value['description_header']]['unit'] =    $unit;
 
 
                 }
