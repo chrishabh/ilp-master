6951a9fcabb8e73138e258955211aab7ff2101bf
rchoudhary-at-770608018768 <ch.rishabh8527@gmail.com>
2024-06-24 23:43:38 +0530

    Update

--- app/Models/BlockDetails.php
+++ app/Models/BlockDetails.php
@@ -22,7 +22,7 @@ class BlockDetails extends Model
         $current_page = $request['page_number'] ?? 1;
         $offset = ($current_page*$noOfRecord)-$noOfRecord;
 
-        $data = BlockDetails::whereNull('deleted_at')->whereNull('project_id')->ORwhere('project_id',$request['project_id'])->offset($offset)->limit($noOfRecord)->get();
+        $data = BlockDetails::whereNull('deleted_at')->where('project_id',$request['project_id'])->offset($offset)->limit($noOfRecord)->get();
 
         if(count($data)>0){
             return $data->toArray();
